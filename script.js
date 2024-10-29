c5a90230d6367a3cde7c367ca2300657545ed49c
Gabriel Souza <gabrielsouza_1909@hotmail.com>
2024-10-29 15:34:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const baseUrl = 'https://gbrlsouza.github.io/Post/';
+const baseUrl = 'http://127.0.0.1:5000';
 const postForm = document.getElementById('postForm');
 const postsContainer = document.getElementById('postsContainer');
 
@@ -64,3 +64,37 @@ postForm.addEventListener('submit', async function (event) {
 });
 
 carregarPosts();
+
+function exibirPosts(posts) {
+    postsContainer.innerHTML = '';
+    posts.forEach(post => {
+        const postCard = document.createElement('div');
+        postCard.classList.add('col');
+        postCard.innerHTML = `
+            <div class="card h-100" style="cursor: pointer;">
+                <img src="${post.imagem}" class="card-img-top" alt="Imagem do Post">
+                <div class="card-body">
+                    <h5 class="card-title">${post.titulo}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${post.subtitulo}</h6>
+                    <p class="card-text"><strong>Assunto:</strong> ${post.assunto}</p>
+                    <p class="card-text">${post.conteudo.slice(0, 100)}...</p> <!-- Mostra uma prévia do conteúdo -->
+                </div>
+            </div>
+        `;
+
+        postCard.addEventListener('click', () => {
+            document.getElementById('modalImage').src = post.imagem;
+            document.getElementById('modalTitle').innerText = post.titulo;
+            document.getElementById('modalSubtitle').innerText = post.subtitulo;
+            document.getElementById('modalSubject').innerText = `Assunto: ${post.assunto}`;
+            document.getElementById('modalContent').innerText = post.conteudo;
+            document.getElementById('modalDate').innerText = `Publicado em: ${post.data_hora}`;
+
+            const modal = new bootstrap.Modal(document.getElementById('postModal'));
+            modal.show();
+        });
+
+        postsContainer.appendChild(postCard);
+    });
+}
+
