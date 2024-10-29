3b2d154df4bf9973794cf7cccd17bf2f3c0887f9
Gabriel Souza <gabrielsouza_1909@hotmail.com>
2024-10-29 15:28:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const baseUrl = window.location.origin;
+const baseUrl = 'https://gbrlsouza.github.io/Post/';
 const postForm = document.getElementById('postForm');
 const postsContainer = document.getElementById('postsContainer');
 
